b0d20d085dd70777cb59ec765e9f7abd15ccb5f4
Tim Van Cauwenberge <cloutking619@gmail.com>
2021-02-12 11:33:33 -0800

    First Commit - Added SptifyAPI and KeychainAccess Swift Dependencies, Updated info.plist

---
+++ Bump/BumpBetaApp.swift
@@ -0,0 +1,24 @@
+//
+//  BumpBetaApp.swift
+//  Bump
+//
+//  Created by Timmy Van Cauwenberge on 2/12/21.
+//
+
+import SwiftUI
+import Combine
+import SpotifyWebAPI
+
+//@main
+//struct BumpBetaApp: App {
+//
+//    @StateObject var spotify = Spotify()
+//
+//    var body: some Scene {
+//        WindowGroup {
+//            RootView()
+//                .environmentObject(spotify)
+//        }
+//    }
+//    
+//}
---
+++ Bump/Model/Spotify.swift
@@ -0,0 +1,8 @@
+//
+//  Spotify.swift
+//  Bump
+//
+//  Created by Timmy Van Cauwenberge on 2/12/21.
+//
+
+import Foundation
